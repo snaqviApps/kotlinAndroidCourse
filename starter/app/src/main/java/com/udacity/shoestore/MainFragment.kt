bf7e73bcb746f71b73a781a6e7d1c0d7cea520db
Sayyid A Naqvi <naqvi.sdsu@gmail.com>
2020-11-12 02:54:54 -0600

    MainFragment: two - click / delayed solution for handling two buttons onClick()

--- starter/app/src/main/java/com/udacity/shoestore/MainFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainFragment.kt
@@ -5,41 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentMainBinding
-import timber.log.Timber
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
 class MainFragment : Fragment() {
 
+    lateinit var binding: FragmentMainBinding
+    lateinit var clickableViews: List<View>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        val binding: FragmentMainBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_main, container, false)
 
-       /**
-        *  1st, 2nd approach
-       binding.buExistLogin.setOnClickListener {
-            view: View ->
-            Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_welcomeFragment)      1st approach
-            view.findNavController().navigate(R.id.action_mainFragment_to_welcomeFragment)                2nd approach
-        }
-        */
-        
         /**
-         * 3rd approach
+         * One - click solution
+         * ***
+            binding.buExistLogin.setOnClickListener(
+                    Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_welcomeFragment)    //    3rd approach
+            )
+            binding.buNewLogin.setOnClickListener(
+                    Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_welcomeFragment)    //    3rd approach
+            )
+         *
          */
-        binding.buExistLogin.setOnClickListener(
-                Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_welcomeFragment)    //    3rd approach
-        )
 
-    return binding.root
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        clickableViews = listOf(buNewLogin, buExistLogin)
+        for (item in clickableViews) {
+            item.setOnClickListener { sendToWelcome(it) }
+        }
+    }
+
+    /** Two - click approach */
+    private fun sendToWelcome(view: View?) {
+        when(view?.id){
+            R.id.buExistLogin -> binding.buExistLogin.setOnClickListener(
+                    Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_welcomeFragment)    //    3rd approach
+            )
+
+            R.id.buNewLogin -> binding.buNewLogin.setOnClickListener(
+                    Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_welcomeFragment)    //    3rd approach
+            )
+        }
     }
 
 }
